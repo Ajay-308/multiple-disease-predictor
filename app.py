9cfd3460e1b740895eab4ed36cd53ec56ea55dd7
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-12-07 12:09:21 +0530

    phpmyadmin connected

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = "2101MM"
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "2101JanM"
+app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "PATIENTS"
 
 mysql = MySQL(app)
