5c2a4714c11dc1caaab87a96b5529aad91c02d08
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-12-13 23:03:36 +0530

    db4free.net

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ import numpy as np
 app = Flask(__name__)
 
 app.secret_key = "2101MM"
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "PATIENTS"
+app.config["MYSQL_HOST"] = "db4free.net"  # Update this with the actual host
+app.config["MYSQL_USER"] = "rock123"
+app.config["MYSQL_PASSWORD"] = "rock1234"
+app.config["MYSQL_DB"] = "rock123"
 
 mysql = MySQL(app)
 
